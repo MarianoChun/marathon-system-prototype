e0f2cc3d50ac73344717b5be05420941b1e9e4ba
Maitena Escalada <maitena_escalada@hotmail.com>
2022-10-19 23:58:00 +0000

    Update eventos.js, style.css

--- eventos.js
+++ eventos.js
@@ -5,7 +5,9 @@ document.addEventListener("DOMContentLoaded", function() {
 document.getElementById("sponsor-propio").addEventListener('click', function(){
     var inputNombreSponsor = document.getElementById("nombre-sponsor");
     var inputDescripcionSponsor = document.getElementById("descripcion");
-
+    var datos = document.getElementById("datos-sponsor-propio");
+    
+    datos.style.backgroundColor="white";
     inputNombreSponsor.disabled = false;
     inputDescripcionSponsor.disabled = false;
 });
@@ -13,7 +15,9 @@ document.getElementById("sponsor-propio").addEventListener('click', function(){
 document.getElementById("sponsor-general").addEventListener('click', function(){
     var inputNombreSponsor = document.getElementById("nombre-sponsor");
     var inputDescripcionSponsor = document.getElementById("descripcion");
-
+    var datos = document.getElementById("datos-sponsor-propio");
+    
+    datos.style.backgroundColor="grey";
     inputNombreSponsor.disabled = true;
     inputDescripcionSponsor.disabled = true;
 });
