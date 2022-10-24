31c926b9fed62f5fb8d4e255c8a3ad101c36917f
Mariano Chun <marianochun01@gmail.com>
2022-10-24 00:28:11 -0300

    Add datos centros salud y marcadores de centros en mapa

---
+++ centrosSalud.js
@@ -0,0 +1,53 @@
+let centrosSalud =
+[
+        {
+            "nombre": "Hospital Dr. Alberto Duhau",
+            "coordenadas": {
+                "x": "-34.52256858098356",
+                "y": "-58.75243614537685"
+            },
+        },
+        {
+            "nombre": "Hospital San Miguel",
+            "coordenadas": {
+                "x": "-34.53480157854912",
+                "y": "-58.72033547055928"
+            },
+        },
+        {
+            "nombre": "Hospital Santa Maria",
+            "coordenadas": {
+                "x": "-34.56237226389738",
+                "y": "-58.73715828384277"
+            },
+        },
+        {
+            "nombre": "Hospital Militar Dr Juan Madera",
+            "coordenadas": {
+                "x": "-34.54842920267654",
+                "y": "-58.672804140488026"
+            },
+        },
+        {
+            "nombre": "Hospital Domingo Angio",
+            "coordenadas": {
+                "x": "-34.53570327403237",
+                "y": "-58.77116610350082"
+            },
+        },
+        {
+            "nombre": "Hospital Mercante",
+            "coordenadas": {
+                "x": "-34.514625994769276",
+                "y": "-58.739981717852196"
+            },
+        },
+        {
+            "nombre": "Hospital Polo Malvinas",
+            "coordenadas": {
+                "x": "-34.51966669147202",
+                "y": "-58.717154207210484"
+            },
+        }
+];
+
--- leaflet.js
+++ leaflet.js
@@ -1,10 +1,32 @@
-
-
-var map = L.map('map').setView([-34.52, -58.70], 13);
+var capaCentros = L.layerGroup([]);
+var mapa = L.map('map').setView([-34.52, -58.70], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+}).addTo(mapa);
 
+
+document.getElementById("btn-mapa-centrosSalud").addEventListener("click", function () {
+    dibujarMapaCentrosSalud(centrosSalud);
+});
+
+document.getElementById("btn-mapa-corredores").addEventListener("click", function () {
+    dibujarMapaCorredores();
+});
+
+
+function dibujarMapaCentrosSalud(centrosSalud) {
+    centrosSalud.forEach(centro => {
+        var marker = L.marker([centro.coordenadas.x, centro.coordenadas.y]).addTo(mapa);
+        marker.bindPopup("<b>" + centro.nombre + "</b>").openPopup();
+        capaCentros.addLayer(marker);
+    });
+
+    capaCentros.addTo(mapa);
+}
+
+function dibujarMapaCorredores() {
+    capaCentros.clearLayers();
+}
 
