7dc50d2848f6c3c928ab0582cd0a7c65fa6fa8aa
Mariano Chun <marianochun01@gmail.com>
2022-10-23 21:32:31 -0300

    Fix sombreado en inputs sponsors de form

--- eventos.js
+++ eventos.js
@@ -5,21 +5,23 @@ document.addEventListener("DOMContentLoaded", function() {
 document.getElementById("sponsor-propio").addEventListener('click', function(){
     var inputNombreSponsor = document.getElementById("nombre-sponsor");
     var inputDescripcionSponsor = document.getElementById("descripcion");
-    var datos = document.getElementById("datos-sponsor-propio");
-    
-    datos.style.backgroundColor="white";
+
     inputNombreSponsor.disabled = false;
     inputDescripcionSponsor.disabled = false;
+
+    inputNombreSponsor.style.backgroundColor="white";
+    inputDescripcionSponsor.style.backgroundColor="white";
 });
 
 document.getElementById("sponsor-general").addEventListener('click', function(){
     var inputNombreSponsor = document.getElementById("nombre-sponsor");
     var inputDescripcionSponsor = document.getElementById("descripcion");
-    var datos = document.getElementById("datos-sponsor-propio");
-    
-    datos.style.backgroundColor="grey";
+
     inputNombreSponsor.disabled = true;
     inputDescripcionSponsor.disabled = true;
+
+    inputNombreSponsor.style.backgroundColor="rgb(187, 185, 185)";
+    inputDescripcionSponsor.style.backgroundColor="rgb(187, 185, 185)";
 });
 
 function enviarFormulario(evento){
