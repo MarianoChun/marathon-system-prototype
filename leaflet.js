971b95d914df31794a8acf52653f8614b8d5e8d8
Mariano Chun <marianochun01@gmail.com>
2022-10-23 22:58:05 -0300

    Add leaflet (mapa)

---
+++ leaflet.js
@@ -0,0 +1,10 @@
+
+
+var map = L.map('map').setView([-34.52, -58.70], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+
