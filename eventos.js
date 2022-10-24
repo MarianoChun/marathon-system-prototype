eb95ac7cab8e2ed02d2bf6ed453847d25260fb7f
Mariano Chun <marianochun01@gmail.com>
2022-10-23 21:40:21 -0300

    Refactor habilitar/deshabilitar inputs form

--- eventos.js
+++ eventos.js
@@ -3,26 +3,29 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 document.getElementById("sponsor-propio").addEventListener('click', function(){
-    var inputNombreSponsor = document.getElementById("nombre-sponsor");
-    var inputDescripcionSponsor = document.getElementById("descripcion");
-
-    inputNombreSponsor.disabled = false;
-    inputDescripcionSponsor.disabled = false;
-
-    inputNombreSponsor.style.backgroundColor="white";
-    inputDescripcionSponsor.style.backgroundColor="white";
+    habilitarInput("nombre-sponsor");
+    habilitarInput("descripcion");
 });
 
 document.getElementById("sponsor-general").addEventListener('click', function(){
-    var inputNombreSponsor = document.getElementById("nombre-sponsor");
-    var inputDescripcionSponsor = document.getElementById("descripcion");
-
-    inputNombreSponsor.disabled = true;
-    inputDescripcionSponsor.disabled = true;
-
-    inputNombreSponsor.style.backgroundColor="rgb(187, 185, 185)";
-    inputDescripcionSponsor.style.backgroundColor="rgb(187, 185, 185)";
+    deshabilitarInput("nombre-sponsor");
+    deshabilitarInput("descripcion");
 });
+
+
+function habilitarInput(idElementoInput){
+    var input = document.getElementById(idElementoInput);
+    
+    input.disabled = false;
+    input.style.backgroundColor="white";
+}
+
+function deshabilitarInput(idElementoInput){
+    var input = document.getElementById(idElementoInput);
+    
+    input.disabled = true;
+    input.style.backgroundColor="rgb(187, 185, 185)";
+}
 
 function enviarFormulario(evento){
     evento.preventDefault(); 
