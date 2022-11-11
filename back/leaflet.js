45036945c63c7981d5121c762ef3abd0cb3f30ec
maitte <maitena_escalada@hotmail.com>
2022-11-11 15:39:04 -0300

    update style de los mapas

--- back/leaflet.js
+++ back/leaflet.js
@@ -31,7 +31,7 @@ document.getElementById("btn-mapa-centrosSalud").addEventListener("click", funct
 });
 
 document.getElementById("btn-mapa-corredores").addEventListener("click", function () {
-    onloadPage();
+    onloadPage(); /*no funciona*/
     habilitarMapaCorredores();
     cambiarATablaCorredores();
 });
--- back/preloader.js
+++ back/preloader.js
@@ -3,4 +3,15 @@ window.onload = function() {
     $('body').removeClass('ocultar-preloader');
 }
 
+document.addEventListener("DOMContentLoaded", function() {
+    onloadPage();
+});
+
+function onloadPage() {
+    window.onload = function() {
+        $('#cargando').fadeOut(2000);
+        $('body').removeClass('ocultar-preloader');
+    }
+}
+
 
