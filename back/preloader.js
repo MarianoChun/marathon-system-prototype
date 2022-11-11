f9cf52bf80a976420c832d9da77d7b55530e4309
maitte <maitena_escalada@hotmail.com>
2022-11-11 14:19:41 -0300

    fix preloader

--- back/preloader.js
+++ back/preloader.js
@@ -1,18 +1,6 @@
 window.onload = function() {
-    $('#cargando').fadeOut(2000);
+    $('#cargando').fadeOut(1000);
     $('body').removeClass('ocultar-preloader');
-}
-
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("finscripcion").addEventListener('submit', enviarFormulario);
-    onloadPage();
-});
-
-function onloadPage() {
-    window.onload = function() {
-        $('#cargando').fadeOut(2000);
-        $('body').removeClass('ocultar-preloader');
-    }
 }
 
 
